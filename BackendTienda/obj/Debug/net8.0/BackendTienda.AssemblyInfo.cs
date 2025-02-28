50eb9a05f4761efda036b24b99e58343096b1bdc
Thiahho <thiagobrizuelav@gmail.com>
2025-02-28 12:18:57 -0300

    28022025

--- BackendTienda/obj/Debug/net8.0/BackendTienda.AssemblyInfo.cs
+++ BackendTienda/obj/Debug/net8.0/BackendTienda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendTienda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e783335e3f80aadd6a6402885decf2aee95c505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcfc3824a271e2ae6f4ef027f4f13ac6f300b56d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendTienda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendTienda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
